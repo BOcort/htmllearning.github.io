90dd4bf793aa6e29683e3fc9fb4d84473263a4b9
BOcort <70442926+BOcort@users.noreply.github.com>
2023-01-25 21:59:10 +0900

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,19 @@
 function dataini() {
     const angka = document.getElementById('angka').value;;
     let hasil = "";
-
-    if (angka % 3 === 0) {
-        hasil = hasil + "Fizz"; 
+    if (angka !== 0) {
+        if (angka % 3 === 0) {
+            hasil = hasil + "Fizz"; 
+        }
+    
+        else if (angka % 5 === 0) {
+            hasil += "Buzz";
+        } else {
+            hasil += "not found"
+        }
+        
     }
-
-    if (angka % 5 === 0) {
-        hasil += "Buzz";
-    }
+    
 
     const tulis = document.getElementById('hasil');
     const fontype = document.createElement('h4');
